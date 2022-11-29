a8d15f1ecf21c8dd01139e55bcd7f02c57106a01
silasiscool <92451943+silasiscool@users.noreply.github.com>
2022-11-29 12:52:06 -0500

    Add files via upload

--- dev-version/MCHS Schedule/popup/webpage-adapt/webpage-adapt.js
+++ dev-version/MCHS Schedule/popup/webpage-adapt/webpage-adapt.js
@@ -10,9 +10,38 @@ if (window.chrome && chrome.runtime && chrome.runtime.id) {
   console.log('Running in Web enviorment');
 
   // Dev
-  Notification.requestPermission().then(()=>{notif = new Notification('true')}).then(() => {
-    console.log(notif);
-  })
+  // request permission on page load
+  document.addEventListener('DOMContentLoaded', function() {
+    if (!Notification) {
+      alert('Desktop notifications not available in your browser. Try Chromium.');
+      return;
+    }
+
+    if (Notification.permission !== "granted")
+      Notification.requestPermission();
+  });
+
+  function notifyMe() {
+    if (Notification.permission !== "granted")
+      Notification.requestPermission();
+    else {
+      var notification = new Notification('Notification title', {
+        icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+        body: "Hey there! You've been notified!",
+      });
+
+      notification.onclick = function() {
+        window.open("http://stackoverflow.com/a/13328397/1269037");
+      };
+    }
+  }
+
+  document.getElementsByTagName('a').forEach((item, i) => {
+    item.addEventListener('click', () => {
+      notifyMe()
+    })
+  });
+
   // End Dev
 
   // Set default options
