39521e21c2e8b46f881d94c9011848b0fc4ecb54
Silas Bartol <92451943+silasiscool@users.noreply.github.com>
2024-03-28 09:55:46 -0400

    Delete schedule-config directory

--- schedule-config/js/index.js
+++
@@ -1,8 +0,0 @@
-config_file.then((savedConfig) => { // open saved config file
-  // load config file
-  if (!sessionStorage.getItem('configFile')) {
-    sessionStorage.setItem('configFile',JSON.stringify(savedConfig))
-  }
-  
-  let config = JSON.parse(sessionStorage.getItem('configFile'));
-})
